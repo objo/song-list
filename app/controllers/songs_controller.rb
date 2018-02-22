449777223bd3eb5f6b1b55de1bbf24b90c19c846
Joe OBrien <joe@objo.com>
2018-02-21 20:26:40 -0500

    deleting shit

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -17,6 +17,12 @@ class SongsController < ApplicationController
     end
   end
   
+  def destroy
+    song = Song.find(params["id"])
+    song.destroy
+    redirect_to song_list_path
+  end
+  
   private 
   
   def song_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  root 'songs#list'
+  
   get 'songs/list' => 'songs#list', as: :song_list
   get 'songs/new' => 'songs#new', as: :new_song
   post 'songs/create' => 'songs#create', as: :songs
+  
+  delete 'songs/destroy/:id' => 'songs#destroy', as: :delete_song
 end
