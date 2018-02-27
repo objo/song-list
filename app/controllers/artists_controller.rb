d2e5b76060329b3f6d54147c4fc1dc68766af30d
Joe OBrien <joe@objo.com>
2018-02-26 19:13:34 -0500

    adding in artists

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,5 @@
+class ArtistsController < ApplicationController
+  def index
+    # retrieve all Artists
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   root 'songs#list'
-  
   get 'songs/list' => 'songs#list', as: :song_list
   get 'songs/new' => 'songs#new', as: :new_song
   post 'songs/create' => 'songs#create', as: :songs
-  
   delete 'songs/destroy/:id' => 'songs#destroy', as: :delete_song
   get 'songs/edit/:id' => 'songs#edit', as: :edit_song
+  post 'songs/update/:id' => 'songs#update', as: :song
   
-  post 'songs/update/:id' => 'songs#update', as: :song 
+  resources :artists
 end
